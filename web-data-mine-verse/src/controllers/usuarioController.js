25f00242ed0dcc91d96d047a6595c2daddebab86
samussouza <samuel.ssouza@sptech.school>
2024-05-19 05:12:57 -0300

    subindo melhorias na home page e criação da pagina sobre mim

--- web-data-mine-verse/src/controllers/usuarioController.js
+++ web-data-mine-verse/src/controllers/usuarioController.js
@@ -54,7 +54,7 @@ function cadastrar(req, res) {
     }  else {
 
         // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
-        usuarioModel.cadastrar(nome, email, senha, tempoDeJogador)
+        usuarioModel.cadastrar(nome, email, senha)
             .then(
                 function (resultado) {
                     res.json(resultado);
